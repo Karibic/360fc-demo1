eb211720e32ccd73aff0ee238493140a07413c73
wenqiyuan <wenqiyuan@360.cn>
2024-10-22 20:06:09 +0800

    修改：     server.py

--- server.py
+++ server.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def print_files_in_directory(directory):
+    for entry in os.scandir(directory):
+        if entry.is_file():
+            print(entry.path)
+        elif entry.is_dir():
+            print_files_in_directory(entry.path)
+
 @app.route('/')
 def hello_world():
+    print_files_in_directory('/mnt/')
     return render_template('/mnt/data/index.html')
 
 if __name__ == '__main__':
